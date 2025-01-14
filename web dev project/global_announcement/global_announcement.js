ce7903ae5f24163925b8960fd8937381f21a2712
RenGubatanga <163238105+RenGubatanga@users.noreply.github.com>
2025-01-14 22:22:57 +0800

    Add files via upload

---
+++ web dev project/global_announcement/global_announcement.js
@@ -0,0 +1,10 @@
+function showTab(tabId) {
+    const tabs = document.querySelectorAll('.tab');
+    const tabContents = document.querySelectorAll('.tab-content');
+  
+    tabs.forEach(tab => tab.classList.remove('active'));
+    tabContents.forEach(content => content.classList.remove('active'));
+  
+    document.querySelector(#${tabId}).classList.add('active');
+    document.querySelector(.tab[onclick="showTab('${tabId}')"]).classList.add('active');
+  }
